ada32394caa2cd70ee4d27774776417bedeb602d
ta <alt3753.7@gmail.com>
2022-06-22 08:30:30 +0700

    test

--- src/main.nim
+++ src/main.nim
@@ -2,7 +2,7 @@ import akane
 import tg
 import gen
 
-var server = newServer()
+var server = newServer("127.0.0.1", 5000)
 server.pages:
   equals("/tg", HttpGet):
     await request.send(generate(tg.tg()))
